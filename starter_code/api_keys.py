d7ca9727fdf321b693621e3afc39e444b01ebbd3
Beccapwd <rjstilley@gmail.com>
2021-02-27 18:50:54 -0600

    Finished Code for WeatherPy

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "1798abba1a9f1dbd7feee943bf7ab2cd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAu_PZy7xWinCCDk9s3Bdmky6tzTQoY4d8"
